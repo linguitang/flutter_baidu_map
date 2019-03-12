df690193aa8b32220b70b4f7e557012ab5dda741
linguitang <935486865@qq.com>
2019-03-12 21:31:17 +0800

    删除没用的引用

--- test/flutter_baidu_map_test.dart
+++ test/flutter_baidu_map_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_baidu_map/flutter_baidu_map.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_baidu_map');
